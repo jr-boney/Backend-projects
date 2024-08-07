caa6de4e9b431a476dbd713a8e2c39879e36112c
Aswin krishna <85611393+jr-boney@users.noreply.github.com>
2024-08-08 00:11:45 +0530

    Delete src directory

--- src/pages/DeleteBook.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DeleteBook = () => {
-  return <div></div>;
-};
-
-export default DeleteBook;
--- src/pages/EditBook.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EditBook = () => {
-  return <div></div>;
-};
-
-export default EditBook;
